26da1525a77020fee2a319874eeedbbf75a346c6
amolwabale <a.m.wabale@gmail.com>
2026-01-27 22:56:11 +0530

    fixed the console error warning

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import { StatusBar, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { en, registerTranslation } from 'react-native-paper-dates';
 
 import AppNavigator from './app/AppNavigator';
+
+// Register locales once for react-native-paper-dates
+registerTranslation('en', en);
 
 export default function App() {
   const isDarkMode = useColorScheme() === 'dark';
